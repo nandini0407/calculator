39c285e900e8a1d52426d644e8dd5f37fef86ee9
nandini0407 <nandini0407@gmail.com>
2017-05-17 17:24:37 -0700

    add basic logic to react calculator

---
+++ react_calculator/calculator.jsx
@@ -0,0 +1,154 @@
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      first: "",
+      second: "",
+      operator: "",
+    };
+  }
+
+  handleOperator(val) {
+    switch (val) {
+      case "4":
+        return "+";
+      case "3":
+        return "-";
+      case "2":
+        return "*";
+      case "1":
+        return "/";
+    }
+  }
+
+  performOperation() {
+    let result;
+    switch (this.state.operator) {
+      case "+":
+        result = this.add();
+        break;
+      case "-":
+        result = this.subtract();
+        break;
+      case "*":
+        result = this.multiply();
+        break;
+      case "/":
+        result = this.divide();
+        break;
+    }
+    this.setState({
+      first: result,
+      second: "",
+      operator: "",
+    });
+  }
+
+  evaluateEquals(e) {
+    e.preventDefault();
+    console.log(this.state.first);
+    if (this.state.second === "") {
+      this.setState({
+        operator: ""
+      });
+    } else {
+      this.performOperation();
+    }
+  }
+
+  evaluateDigit(e) {
+    e.preventDefault();
+    console.log(this.state.first);
+    if (this.state.operator === "") {
+      this.setState({ first: this.state.first += e.target.value });
+    } else {
+      this.setState({ second: this.state.second += e.target.value });
+    }
+  }
+
+  evaluateOperator(e) {
+    e.preventDefault();
+    console.log(this.state.first);
+    let operator = this.handleOperator(e.target.value);
+    if (this.state.operator === "") {
+      this.setState({ operator: operator });
+    } else {
+      this.performOperation();
+      this.setState({
+        operator: operator
+      });
+    }
+  }
+
+  clearScreen(e) {
+    e.preventDefault();
+    this.setState({
+      first: "",
+      second: "",
+      operator: ""
+    });
+  }
+
+  add() {
+    return String(Number(this.state.first) + Number(this.state.second));
+  }
+
+  subtract() {
+    return String(Number(this.state.first) - Number(this.state.second));
+  }
+
+  multiply() {
+    return String(Number(this.state.first) * Number(this.state.second));
+  }
+
+  divide() {
+    if (this.state.second === "0") {
+      return "Zero Division!";
+    } else {
+      return String((Number(this.state.first) / Number(this.state.second)).toFixed(2));
+    }
+  }
+
+  // values in the li elements corresond to the follwing :
+  // 1: divide, 2: multiply, 3: subtract, 4: add, 5: equals(=), 6: decimal point
+  render() {
+    return (
+      <div className="calculator">
+        <div className="screen">{ this.state.first }</div>
+        <div className="keyPad">
+          <ul className="calc-btn-row">
+            <li key={1} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="7" >7</li>
+            <li key={2} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="8" >8</li>
+            <li key={3} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="9" >9</li>
+            <li key={4} className="btn lst operator" onClick={ this.evaluateOperator.bind(this) } value="1" >/</li>
+          </ul>
+          <ul className="calc-btn-row">
+            <li key={1} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="4" >4</li>
+            <li key={2} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="5" >5</li>
+            <li key={3} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="6" >6</li>
+            <li key={4} className="btn lst operator" onClick={ this.evaluateOperator.bind(this) } value="2" >*</li>
+          </ul>
+          <ul className="calc-btn-row">
+            <li key={1} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="1" >1</li>
+            <li key={2} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="2" >2</li>
+            <li key={3} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="3" >3</li>
+            <li key={4} className="btn lst operator" onClick={ this.evaluateOperator.bind(this) } value="3" >-</li>
+          </ul>
+          <ul className="last-row">
+            <ul key={1} className="left-last-row lst">
+              <li key={1} className="btn clear" onClick={ this.clearScreen.bind(this) } value="C" >C</li>
+              <li key={2} className="btn decimal" onClick={ this.evaluateDigit.bind(this) } value="6" >.</li>
+            </ul>
+            <li key={2} className="btn lst number" onClick={ this.evaluateDigit.bind(this) } value="0" >0</li>
+            <li key={3} className="btn lst operator" onClick={ this.evaluateEquals.bind(this) } value="5" >=</li>
+            <li key={4} className="btn lst operator" onClick={ this.evaluateOperator.bind(this) } value="4" >+</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
